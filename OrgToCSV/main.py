a6405c44586735cccebe42af9695540a50517d7b
mitchellluhm <sharperacc@gmail.com>
2018-03-11 19:39:16 -0500

    extracts subject name

--- OrgToCSV/main.py
+++ OrgToCSV/main.py
@@ -1,4 +1,30 @@
 import subject
 
-c2 = subject.Subject("STAT 3021")
-print(c2.name)
+
+# ** CSCI 1001 [/] -> CSCI 1001
+def get_subject_name(name):
+    start_index = str.find(name, "* ") + 2
+    end_index = str.find(name, " [")
+
+    if end_index < 0:
+        end_index = len(name)
+
+    return name[start_index:end_index]
+
+
+# open todo.txt for reading
+f = open('todo.txt', 'r')
+line = f.readline()
+last_line = line
+while len(line) > 0:
+    # check if current line has ` :LOGBOOK: `
+    if str.find(line, ":LOGBOOK:") > -1:
+        # found a logbook
+        subject_name = get_subject_name(last_line)
+
+        print("Found a logbook")
+
+
+    last_line = line
+    line = f.readline()
+f.close()
